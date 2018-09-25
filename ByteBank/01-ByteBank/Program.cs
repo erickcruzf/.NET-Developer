353cd08e86bc185f0dd00258f36eeac5d8d5d493
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2018-09-24 22:11:25 -0300

    C# Part 2

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 9080908;
+            contaDaGabriela.saldo = 80.924;
+
+            Console.WriteLine("Conta: " + contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Numero: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo.ToString("c2"));
+
+            Console.WriteLine("Processando. . . Aguarde um instante. . .");
+            contaDaGabriela.saldo += 200;
+
+            Console.WriteLine("Novo saldo: " + contaDaGabriela.saldo.ToString("c2"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            ContaCorrente contaDoJoao = new ContaCorrente();
+            contaDoJoao.titular = "Gabriela";
+            contaDoJoao.agencia = 863;
+            contaDoJoao.numero = 863452;
+
+            Console.WriteLine(contaDaGabriela == contaDoJoao); //False comaparação de referência
+
+            Console.WriteLine(5 == 5); //True comparação de valor
+
+            contaDaGabriela = contaDoJoao;
+            contaDaGabriela.saldo += 300;
+
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDoJoao.saldo);
+
+            if (contaDaGabriela.saldo >= 100)
+            {
+                contaDaGabriela.saldo -= 100;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
