a0800b7942f1d2e768002c393c8f3a4c0c2723c9
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2018-10-24 14:45:36 -0300

    C# Part 9

---
+++ ByteBankP8/ByteBankImportacaoExportacao/3_CriandoArquivo.cs
@@ -0,0 +1,41 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = $"";
+
+                var encoding = Encoding.UTF8;
+
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        static void CriarArquivoComWriter()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                escritor.Write("434,344343,343.9,Pedro");
+            }
+        }
+    }
+}
---
+++ ByteBankP8/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,49 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Tesntando File.WriteAllText");
+
+
+            var bytesArquivos = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"Arquivo contas.txt possui {bytesArquivos.Length} bytes");
+
+
+
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
+
+            foreach(var linha in linhas)
+            {
+                Console.WriteLine(linha);
+            }
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Digite o seu nome:");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine(nome);
+
+            UsarStreamDeEntrada();
+
+            //EscritaBinaria();
+            //LeituraBinaria();
+
+            Console.WriteLine("Aplicação finalizada...");
+
+            Console.ReadLine();
+        }
+    }
+}
