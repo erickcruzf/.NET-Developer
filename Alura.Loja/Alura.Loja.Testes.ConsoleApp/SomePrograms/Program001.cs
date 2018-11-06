0fa777673dccf3b034442c203d0e36a75fbc9e7c
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2018-11-06 21:55:50 -0200

    Entity Framework

---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/SomePrograms/Program001.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp.SomePrograms
+{
+    class Program001
+    {
+        static void OldMain()
+        {
+            //GravarUsandoAdoNet();
+            //GravarUsandoEntity();
+            //RecuperarProdutos();
+            //ExcluirProdutos();
+            //RecuperarProdutos();
+            AtualizarProduto();
+        }
+
+        private static void AtualizarProduto()
+        {
+            // inclui um produto
+            GravarUsandoEntity();
+            RecuperarProdutos();
+
+
+            // atualiza o produto
+            using (var repo = new ProdutoDAOEntity())
+            {
+                Produto primeiro = repo.Produtos().First();
+                primeiro.Nome = "Cassino Royale";
+                repo.Atualizar(primeiro);
+            }
+            RecuperarProdutos();
+        }
+
+        private static void ExcluirProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+                foreach (var item in produtos)
+                {
+                    repo.Remover(item);
+                }
+            }
+        }
+
+        private static void RecuperarProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+                Console.WriteLine($"Foram encontrados {produtos.Count} produtos(s).");
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.PrecoUnitario = 19.89;
+
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                contexto.Adicionar(p);
+            }
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.PrecoUnitario = 19.89;
+
+            //using (var repo = new ProdutoDAO())
+            //{
+            //    repo.Adicionar(p);
+            //}
+        }
+    }
+}
---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/SomePrograms/Program002.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp.SomePrograms
+{
+    class Program002
+    {
+        static void OldMain2()
+        {
+            using (var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var produtos = contexto.Produtos.ToList();
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                //var p1 = produtos.First();
+                //p1.Nome = "Censura"; // O SQL chama um Update();
+
+                //var novoProduto = new Produto()
+                //{
+                //    Nome = "Desinfetante",
+                //    Categoria = "Limpeza",
+                //    Preco = 2.99
+                //};
+                //contexto.Produtos.Add(novoProduto); // O SQL chama um Insert();
+
+                var p1 = produtos.Last();
+                contexto.Produtos.Remove(p1); // O SQL chama um Delete();
+
+                // var entry = contexto.Entry(novoProduto); // Caso adicionado e deletado antes do SaveChanges(); 
+                // podemos perguntar pelo elemento dessa forma, sendo categorizado como 'Detached' pelo entry.State;
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.SaveChanges();
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+            }
+        }
+
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            Console.WriteLine("==========");
+
+            foreach (var e in entries)
+            {
+                Console.WriteLine($"{e.Entity.ToString()} - {e.State}");
+            }
+        }
+    }
+}
