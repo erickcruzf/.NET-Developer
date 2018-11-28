3ec689efa347132be77be1dc67fdf554261ecf9a
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2018-11-28 17:09:27 -0200

    CasaDoCodigo WebSite

---
+++ AspNetCoreP1/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -0,0 +1,85 @@
+﻿using CasaDoCodigo.Controllers;
+using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IPedidoRepository
+    {
+        Pedido GetPedido();
+        void AddItem(string codigo);
+    }
+
+    public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
+    {
+        private readonly IHttpContextAccessor contextAcessor;
+
+        public PedidoRepository(ApplicationContext contexto, IHttpContextAccessor contextAcessor) : base(contexto)
+        {
+            this.contextAcessor = contextAcessor;
+        }
+
+        public void AddItem(string codigo)
+        {
+            var produto = contexto.Set<Produto>()
+                .Where(p => p.Codigo == codigo)
+                .SingleOrDefault();
+
+            if (produto == null)
+            {
+                throw new ArgumentException("Produto não encontrado");
+            }
+
+            var pedido = GetPedido();
+
+            var itemPedido = contexto.Set<ItemPedido>()
+                                .Where(i => i.Produto.Codigo == codigo
+                                    && i.Pedido.Id == pedido.Id)
+                                .SingleOrDefault();
+
+            if (itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                contexto.Set<ItemPedido>()
+                    .Add(itemPedido);
+
+                contexto.SaveChanges();
+            }
+        }
+
+        public Pedido GetPedido()
+        {
+            var pedidoId = GetPedidoId();
+            var pedido = dbSet
+                .Include(p => p.Itens)
+                .ThenInclude(i => i.Produto)
+                .Where(p => p.Id == pedidoId)
+                .SingleOrDefault();
+
+            if (pedido == null)
+            {
+                pedido = new Pedido();
+                dbSet.Add(pedido);
+                contexto.SaveChanges();
+                SetPedidoID(pedido.Id); // Grava na sessão!!
+            }
+
+            return pedido;
+        }
+
+        private int? GetPedidoId() // int? => Significa que pode ser nulo.
+        {
+            return contextAcessor.HttpContext.Session.GetInt32("pedidoId");
+        }
+
+        private void SetPedidoID(int pedidoId) // Gravar o pedidoId na sessão.
+        {
+            contextAcessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
+        }
+    }
+}
