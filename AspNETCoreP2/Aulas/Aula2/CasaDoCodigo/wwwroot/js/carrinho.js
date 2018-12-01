fb47673f75165bf40b167fab5faf10588da2eaba
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2018-12-01 02:11:45 -0200

    Add files via upload

---
+++ AspNETCoreP2/Aulas/Aula2/CasaDoCodigo/wwwroot/js/carrinho.js
@@ -0,0 +1,67 @@
+﻿
+class Carrinho {
+    clickIncremento(button) {
+        let data = this.getData(button);
+        data.Quantidade++;
+        this.postQuantidade(data);
+
+    }
+    clickDecremento(button) {
+        let data = this.getData(button);
+        data.Quantidade--;
+        this.postQuantidade(data);
+    }
+
+    updateQuantidade(input) {
+        let data = this.getData(input);
+        this.postQuantidade(data);
+    }
+
+
+    getData(elemento) {
+        var linhaDoItem = $(elemento).parents('[item-id]');
+        var itemId = $(linhaDoItem).attr('item-id');
+        var novaQuantidade = $(linhaDoItem).find('input').val();
+
+        var data = {
+            Id: itemId,
+            Quantidade: novaQuantidade
+        };
+
+        return data;
+    }
+
+    postQuantidade(data) {
+
+        let token = $('input[name="__RequestVerificationToken"]').val();
+
+        let headers = {};
+        headers['RequestVerificationToken'] = token;
+
+        $.ajax({
+            url: '/pedido/updatequantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            headers: headers
+        }).done(function (response) { //location.reload() é ruim porque recarrega a página inteira
+            let itemPedido = response.itemPedido;
+            let linhaDoItem = $('[item-id=' + itemPedido.id + ']')
+            linhaDoItem.find('input').val(itemPedido.quantidade);
+            linhaDoItem.find('[subtotal]').html((itemPedido.subtotal).duasCasas());
+            let carrinhoViewModel = response.carrinhoViewModel;
+            $('[numero-itens]').html('Total: ' + carrinhoViewModel.itens.length + ' itens');
+            $('[total]').html((carrinhoViewModel.total).duasCasas())
+            if (itemPedido.quantidade == 0) {
+                linhaDoItem.remove();
+            }
+        });
+    }
+}
+
+var carrinho = new Carrinho();
+
+// função pra arrendor o valor em duas casas decimais.
+Number.prototype.duasCasas = function () {
+    return this.toFixed(2).replace('.', ',');
+}
