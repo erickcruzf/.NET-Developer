9ceea0df46945cd95dfb84eacab8eb894eee7640
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2019-02-06 20:01:18 -0200

    Projeto final

---
+++ ProjetoFinalDotNetJunior/AluraNetJunior-master/CasaDoCodigo/Models/ViewModels/BuscaDeProdutoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class BuscaDeProdutoViewModel
+    {
+        public string Pesquisa { get; set; }
+        public IList<Produto> Produtos { get; set; }
+
+        public BuscaDeProdutoViewModel()
+        {
+        }
+        public BuscaDeProdutoViewModel(string pesquisa, IList<Produto> produtos)
+        {
+            this.Pesquisa = pesquisa;
+            this.Produtos = produtos;
+        }
+    }
+}
