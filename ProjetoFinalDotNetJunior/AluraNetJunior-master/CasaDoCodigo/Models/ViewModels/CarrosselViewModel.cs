d752ac16a0b27ff220ea33c6e7316a8eb35948fe
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2019-02-11 15:14:01 -0200

    Projeto Final

---
+++ ProjetoFinalDotNetJunior/AluraNetJunior-master/CasaDoCodigo/Models/ViewModels/CarrosselViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class CarrosselViewModel
+    { 
+        public IList<Produto> Produtos { get; set; }
+
+        public CarrosselViewModel()
+        {
+        }
+        public CarrosselViewModel(IList<Produto> produtos)
+        {
+            this.Produtos = produtos;
+        }
+    }
+}
